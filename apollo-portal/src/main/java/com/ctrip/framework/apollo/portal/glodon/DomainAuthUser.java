316f5b155a7d7bef584674f0fb9db0efe1b866f2
liuj-ai <liuj-ai@glodon.com>
2018-01-23 17:30:25 +0800

    add 域账号

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/glodon/DomainAuthUser.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/glodon/DomainAuthUser.java
@@ -22,9 +22,4 @@ public class DomainAuthUser {
 			throw new RuntimeException(ex);
 		}
 	}
-
-//	public static void main(String[] args) {
-//		System.out.println(domainAuth("liuj-ai", "LiuJing19891112"));
-//		System.out.println(domainAuth("bim", "123"));
-//	}
 }
